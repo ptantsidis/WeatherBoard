1c6996bb3f489fe064ef72407028cc83c450f4dc
Philip Tantsidis <ptantsidis@gmail.com>
2021-10-03 15:07:37 -0400

    weather API working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
 let timeDisplayEl = $("#time-display");
 let APIKey = "e0cdfdd3f8280af1549d472c098d3d6e";
 let APICall = "https://api.openweathermap.org/data/2.5/weather?q=";
-//ttps://openweathermap.org/api/one-call-api
 let cityBtn = $("#search-btn");
 let cityList = $("#city-list");
 let searchInput = $("#input");
+// let searchAgainEl = $("")
 let newCity = ("");
+let oldCity = ("");
+let cityArray = [];
+let color = ""
+let city = ""
 
 function displayTime() {
   let currentTime = moment().format("LLLL");
@@ -13,6 +17,16 @@ function displayTime() {
 }
 setInterval(displayTime, 1000);
 
+
+cityList.on("click", function (event) {
+  event.preventDefault();
+
+  console.log(event.target );
+  let citystring = (event.target);
+  console.log( this, arguments.text );
+  // oldCity = this;
+    // currentForecast(oldCity);
+})
 //create list element as required
 cityBtn.on("click", function (event) {
   event.preventDefault();
@@ -21,10 +35,12 @@ cityBtn.on("click", function (event) {
     alert("Try Again");
   } else {
     cityList.append("<li><a href ='#'>" + (searchInput.val() + "</a></li>"));
-    // localStorage.setItem(key.newCity);
+    cityArray.push(newCity);
+    localStorage.setItem("search", JSON.stringify(cityArray));
     currentForecast(newCity);
   }
 })
+
 function currentForecast(cityname) {
   let weatherUrl = APICall + newCity + "&appid=" + APIKey + "&units=imperial";
   $.ajax({
@@ -32,23 +48,14 @@ function currentForecast(cityname) {
     type: "GET"
   })
     .then(function (response) {
-      // if (response.ok) {
-      //   response.json().then(function (data) {
-      console.log(response);
       let lat = response.coord.lat;
       let lon = response.coord.lon;
+      city = response.name;
+      console.log(response)
       callfiveday(lat, lon);
-      $("#1").html(`<div>
-          <h1>City: ${response.name}</h1>
-          <h2>${response.main.temp + " °F"}</h2>
-          <h3>${response.weather[0].description}</h3>
-          <h4>Wind Speed ${response.wind.speed + " mph"}</h4>
-          <span><img src="http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png" />
-          <h5>Humidity: ${response.main.humidity + " %"}</h5>
-          </div>`)
     })
 }
-let color = ""
+
 function callfiveday(lat, lon) {
   let weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIKey}&units=imperial`;
   $.ajax({
@@ -56,36 +63,53 @@ function callfiveday(lat, lon) {
     type: "GET"
   })
     .then(function (response) {
-      // if (response.ok) {
-      //   response.json().then(function (data) {
       console.log(response);
-
-      let daily = response.daily;
+      let oneMap = response.daily;
+      console.log(oneMap);
       let htmlcode = "";
-      for (let i = 0; i < 5; i++) {
-        console.log(daily[i].uvi);
-        if (daily[i].uvi < 3) {
-          let color = "green"
+      for (let i = 0; i < 6; i++) {
+        if (oneMap[i].uvi < 3) {
+          color = "green"
         }
-        else if (daily[i] < 6) {
-          let color = "yellow"
+        else if (oneMap[i].uvi < 6) {
+          color = "yellow"
         }
-        else if (daily[i] < 8) {
-          let color = "orange"
+        else if (oneMap[i].uvi  < 8) {
+          color = "orange"
         }
-        else if (daily[i] < 10) {
-          let color = "red"
+        else if (oneMap[i].uvi  < 11) {
+          color = "red"
         }
         else {
-          let color = "indigo"
+
+          color = "indigo"
         }
-        htmlcode += `<div class="card">
-           <h2>${daily[i].temp.max + " °F"}</h2>
-           <h3>${daily[i].weather[0].description}</h3>
-           <span><img src="http://openweathermap.org/img/wn/${daily[i].weather[0].icon}@2x.png" />
-           <h4 class="${color}">uvi:${daily[i].uvi}</h4>
-           </div>`
+        if (i === 0) {
+          $("#1").html(`<div>
+          <h1>City: ${city}</h1>
+          <h2>${Math.round(response.current.temp) + " °F"}</h2>
+          <h3>${response.current.weather[0].description}</h3>
+          <h4>Wind Speed ${Math.round(response.current.wind_speed) + " mph"}</h4>
+          <span><img src="http://openweathermap.org/img/wn/${response.current.weather[0].icon}@2x.png" />
+          <h5>Humidity: ${response.current.humidity + " %"}</h5>
+          <h5 class ="${color}">uvi: ${response.current.uvi}</h5>
+          </div>`)
+          console.log(oneMap[i]);
+          console.log(color);
+        }
+        else {
+          htmlcode += `<div class="card">
+            <h2>${moment(oneMap[i].dt * 1000).format("ddd")}</h2>
+            <h2>${Math.round(oneMap[i].temp.max) + " °F"}</h2>
+            <h3>${oneMap[i].weather[0].description}</h3>
+            <span><img src="http://openweathermap.org/img/wn/${oneMap[i].weather[0].icon}@2x.png" />
+            <h4 class="${color}">uvi: ${oneMap[i].uvi}</h4>
+            </div>`
+            console.log(color)
+            console.log(oneMap[i]);
+        }
       }
       $("#fiveday").html(htmlcode)
     })
 }
+
